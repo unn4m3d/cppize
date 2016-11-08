8e43dced0677ef998944806e201f5a372869c0c8
unn4m3d <smelnikov871@gmail.com>
2016-11-08 11:22:48 +1000

    Fixed typo

--- src/cppize/nodes/lib_def.cr
+++ src/cppize/nodes/lib_def.cr
@@ -1,7 +1,7 @@
 module Cppize
   class Transpiler
     protected def transpile(node : LibDef, should_return : Bool = false)
-      try_tr node
+      try_tr node do
         @lib_defs.block "namespace #{node.name}" do
         #begin
           @lib_defs.line transpile node.body
