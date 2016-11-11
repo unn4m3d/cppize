a68146b7220d8276c4bf5ae37c0b53eef7318b05
unn4m3d <smelnikov871@gmail.com>
2016-11-11 11:27:50 +1000

    Added support for case

---
+++ src/cppize/nodes/case.cr
@@ -0,0 +1,24 @@
+module Cppize
+  class Transpiler
+    register_node Case do
+      Lines.new do |l|
+        l.line nil
+        l.block("switch(#{transpile node.cond})") do
+          node.whens.each do |wh|
+            wh.conds.each do |cond|
+              l.line "case #{transpile cond}:",true
+            end
+            l.line transpile(wh.body)
+            l.line "break"
+          end
+
+          if node.else
+            l.line "default:",true
+            l.line transpile node.else.not_nil!.body
+            l.line "break"
+          end
+        end
+      end.to_s
+    end
+  end
+end
