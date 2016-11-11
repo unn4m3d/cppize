36095bf377c28bf1310f37f78e5b529d788902e5
unn4m3d <smelnikov871@gmail.com>
2016-11-11 10:26:15 +1000

    Implemented ArrayLiteral

---
+++ src/cppize/nodes/array_literal.cr
@@ -0,0 +1,13 @@
+module Cppize
+  class Transpiler
+    register_node ArrayLiteral do
+      if node.elements.empty? && !node.of && !options[:allow_empty_arrays]?
+        raise Error.new("You must specify array type of empty array literals",node,nil,@current_filename)
+      end
+
+      arr_type = (node.of ? transpile node.of : "decltype(#{transpile node.elements.first})")
+
+      "Crystal::Array< #{arr_type} >{ #{node.elements.map{|x| transpile x}.join(", ")} }"
+    end
+  end
+end
