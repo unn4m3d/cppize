8e3bac759d931ec6aceb7fe78f19c75734a3c4f0
unn4m3d <smelnikov871@gmail.com>
2016-11-11 10:57:37 +1000

    Added support for object-oriented chars

--- src/cppize/nodes/char_literal.cr
+++ src/cppize/nodes/char_literal.cr
@@ -1,7 +1,7 @@
 module Cppize
   class Transpiler
     register_node CharLiteral do
-      "'#{escape_cpp_string node.value.to_s}'"
+      "'#{escape_cpp_string node.value.to_s}'_crchar"
     end
   end
 end
