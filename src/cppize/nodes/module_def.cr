1f5021890dc1cfdd4db45d8d53f1fe6b8e2deb63
unn4m3d <smelnikov871@gmail.com>
2016-11-11 12:37:33 +1000

    Fixed namespace names

--- src/cppize/nodes/module_def.cr
+++ src/cppize/nodes/module_def.cr
@@ -31,8 +31,8 @@ module Cppize
         ""
       else
         Lines.new(@failsafe) do |l|
-          l.line nil 
-          l.block "namespace #{node.name}" do
+          l.line nil
+          l.block "namespace #{node.name.sub(/::$/,'')}" do
             old_namespace, @current_namespace = @current_namespace, @current_namespace + "::" + node.name.to_s
             l.line transpile node.body
             @current_namespace = old_namespace
