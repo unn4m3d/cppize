ba15e15b2b0d3d87577b91a834ff43e687e568f9
unn4m3d <smelnikov871@gmail.com>
2016-11-11 23:24:54 +1000

    Code

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,30 @@
 window.onload = function(){
   var flask = new CodeFlask;
+  var code = "";
   flask.run("#crystal-code-wrapper",{language: "crystal"});
+  flask.onUpdate(function(c){code = c});
+  //Jackbox.init();
+  $("#transpile-button>button").click(function(){
+    $.ajax({
+      method: "POST",
+      data: code,
+      dataType : "json",
+      success : function(data){
+        console.log(data);
+        if(data.error)
+        {
+          $("#output code").text("/*Error ! See console for more info*/");
+        }
+        else
+        {
+          $("#output code").text(data.code);
+        }
+      },
+      error : function(e,ee){
+        console.log(e);
+        console.log(ee);
+        $("#output code").text("/*Error ! See console for more info*/");
+      }
+    });
+  });
 };
