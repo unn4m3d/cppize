6cba1173cf47998e502f915f86aefb87ec4a0b07
unn4m3d <smelnikov871@gmail.com>
2016-11-17 20:39:03 +1000

    Splats in defs

--- src/cppize/nodes/def.cr
+++ src/cppize/nodes/def.cr
@@ -11,13 +11,15 @@ module Cppize
         typenames = [] of String
 
         typenames += node.args.select{|x| !x.restriction}.map{|x| ARG_TYPE_PREFIX + x.name}
-
-        args = node.args.map do |arg|
-          restr = arg.restriction ? transpile arg.restriction : ARG_TYPE_PREFIX + arg.name
-          def_v = (arg.default_value ? " = " + transpile arg.default_value : "")
-          "#{restr} #{arg.name}#{def_v}"
-        end.join(", ")
-
+        _args = [] of String
+        node.args.each_with_index do |arg,i|
+          if node.splat_index && node.splat_index.not_nil! == i
+            _args << transpile(arg,:tuple)
+          else
+            _args << transpile arg
+          end
+        end
+        args = _args.join(", ")
 
         @scopes << Scope.new if @scopes.empty?
         node.args.each do |arg|
@@ -30,7 +32,8 @@ module Cppize
           typenames << "... " + ARG_TYPE_PREFIX + block.name + "_args"
           pref = ARG_TYPE_PREFIX
           b_type = "#{pref}#{block.name}_ret, #{pref}#{block.name}_args..."
-          args += "#{args.empty? ? "" : ","} #{STDLIB_NAMESPACE}::Proc<#{b_type}> #{block.name}"
+          arg_str = "#{args.empty? ? "" : ","} #{STDLIB_NAMESPACE}::Proc<#{b_type}> #{block.name}"
+          args += arg_str
           @scopes.first[block.name] = {symbol_type: :object, value: block}
         end
 
@@ -52,18 +55,20 @@ module Cppize
             typenames += m.sub(/^\</, "").sub(/\>$/, "").split(",").map &.strip
         end
 
+        @current_class.gsub(/\<.+\>/) do |m|
+            typenames += m.sub(/^\</, "").sub(/\>$/, "").split(",").map &.strip
+        end
+
         global_template = ((typenames.size > 0 || @template_defs.includes? template_name) ? "template<#{typenames.map{|x| "typename #{x}"}.join(", ")} > " : "")
 
         namesp = (@current_namespace.empty? ? "" : @current_namespace + "::")
 
         if @in_class
-          Lines.new @failsafe do
-            if @current_visibility != node.visibility
-              l.line "public:", true
-              @current_visibility = node.visibility
-              l.line local_signature
-            end
+          if @current_visibility != node.visibility
+            l.line "public:", true
+            @current_visibility = node.visibility
           end
+          l.line local_signature
 
           global_signature = "#{global_template}#{modifiers}#{def_type} #{namesp}#{@current_class}::#{common_signature}"
 
@@ -94,7 +99,73 @@ module Cppize
             end
           end
         end
+
+        l.line add_def_with_splat node
       end.to_s
+    end
+
+    protected def add_def_with_splat(d : Def)
+      return nil if d.splat_index.nil?
+      typenames = [] of String
+
+      typenames += d.args.select{|x| !x.restriction}.map{|x| ARG_TYPE_PREFIX + x.name}
+
+      idx = d.splat_index.not_nil!
+
+      args = [] of String
+      pass_args = [] of String
+
+      d.args.each_with_index do |x,i|
+            if idx == i
+              args << transpile(x, :splat)
+              pass_args << "#{STDLIB_NAMESPACE}::make_tuple(#{x.name}...)"
+            else
+              args << transpile x
+              pass_args << translate_name x.name
+            end
+      end
+      args_str = args.join(", ")
+
+      namesp = (@current_namespace.empty? ? "" : @current_namespace + "::")
+      def_type = (d.return_type ? transpile d.return_type : "auto")
+
+      modifiers = (d.receiver.to_s == "self" ? "static " : "")
+      local_template = (typenames.size > 0 ? "template<#{typenames.map{|x| "typename #{x}"}.join(", ")} > " : "")
+
+      @current_namespace.gsub(/\<.+\>/) do |m|
+          typenames += m.sub(/^\</, "").sub(/\>$/, "").split(",").map &.strip
+      end
+
+      @current_class.gsub(/\<.+\>/) do |m|
+          typenames += m.sub(/^\</, "").sub(/\>$/, "").split(",").map &.strip
+      end
+      template_name = "#{@current_namespace}::#{@current_class}::#{d.name}"
+
+      global_template = ((typenames.size > 0 || @template_defs.includes? template_name) ? "template<#{typenames.map{|x| "typename #{x}"}.join(", ")} > " : "")
+
+      local_signature = "#{local_template}#{modifiers}#{def_type} #{translate_name d.name}(#{args_str})"
+      if @in_class
+        global_signature = "#{global_template}#{modifiers}#{def_type} #{namesp}#{@current_class}::#{translate_name d.name}(#{args_str})"
+
+        @defs.block global_signature do
+          @defs.line "return #{d.name}(#{pass_args.join(", ")})"
+        end
+        return local_signature
+      else
+        if namesp.empty?
+          @forward_decl_defs.line local_signature
+        else
+          @forward_decl_defs.block "namespace #{namesp}" do
+            @forward_decl_defs.line local_signature
+          end
+        end
+
+        Lines.new @failsafe do |l|
+          l.block(local_signature) do
+            l.line "return #{d.name}(#{pass_args.join(", ")})"
+          end
+        end.to_s
+      end
     end
   end
 end
