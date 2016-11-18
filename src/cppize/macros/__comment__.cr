b359af2b89c4915b79857293f421f81eadd40567
unn4m3d <smelnikov871@gmail.com>
2016-11-18 10:42:27 +1000

    __comment__ macro

--- src/cppize/macros/__comment__.cr
+++ src/cppize/macros/__comment__.cr
@@ -1,9 +1,14 @@
 module Cppize
   class Transpiler
     add_macro(:__comment__) do |_self, call|
-      s = "/*\n"
-      call.args.each { |x| s += x.to_s.gsub(/(?<!\\)"/, "") + "\n" }
-      s + "*/"
+      "/*#{call.args.reduce("") do |memo,arg|
+        m = memo.to_s
+        if arg.responds_to? :value
+          m + arg.value.to_s + "\n"
+        else
+          m + arg.to_s
+        end
+      end}*/"
     end
   end
 end
